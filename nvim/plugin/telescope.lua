c10b86a4aa1ed09cd2a1abc60f9d9d345a184bce
achuie <31836492+achuie@users.noreply.github.com>
2024-11-07 16:36:14 +0900

    Merge personal telescope bindings.

--- nvim/plugin/telescope.lua
+++ nvim/plugin/telescope.lua
@@ -69,40 +69,64 @@ local function fuzzy_grep_current_file_type()
   grep_current_file_type(fuzzy_grep)
 end
 
-vim.keymap.set('n', '<leader>tp', function()
+vim.keymap.set('n', '<leader>sf', function()
   builtin.find_files()
-end, { desc = '[t]elescope find files - ctrl[p] style' })
-vim.keymap.set('n', '<M-p>', builtin.oldfiles, { desc = '[telescope] old files' })
-vim.keymap.set('n', '<C-g>', builtin.live_grep, { desc = '[telescope] live grep' })
-vim.keymap.set('n', '<leader>tf', fuzzy_grep, { desc = '[t]elescope [f]uzzy grep' })
-vim.keymap.set('n', '<M-f>', fuzzy_grep_current_file_type, { desc = '[telescope] fuzzy grep filetype' })
-vim.keymap.set('n', '<M-g>', live_grep_current_file_type, { desc = '[telescope] live grep filetype' })
+end, { desc = '[s]earch [f]iles' })
+vim.keymap.set('n', '<leader>?', builtin.oldfiles, { desc = '[?] Find recently opened files' })
+vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[s]earch via [g]rep' })
+vim.keymap.set('n', '<leader>sG', fuzzy_grep, { desc = '[s]earch fuzzily via [G]rep' })
+vim.keymap.set('n', '<leader>sT', fuzzy_grep_current_file_type, { desc = '[s]earch via grep amongst current file[T]ype' })
+vim.keymap.set('n', '<leader>st', live_grep_current_file_type, { desc = '[s]earch via grep amongst current file[t]ype' })
 vim.keymap.set(
   'n',
-  '<leader>t*',
+  '<leader>s*t',
   grep_string_current_file_type,
-  { desc = '[t]elescope grep current string [*] in current filetype' }
+  { desc = '[s]earch current string [*] amongst current file[t]ype via grep' }
 )
-vim.keymap.set('n', '<leader>*', builtin.grep_string, { desc = '[telescope] grep current string [*]' })
-vim.keymap.set('n', '<leader>tg', project_files, { desc = '[t]elescope project files [g]' })
+vim.keymap.set('n', '<leader>s*g', builtin.grep_string, { desc = '[s]earch current string [*] via [g]rep' })
+vim.keymap.set('n', '<leader>sp', project_files, { desc = '[s]earch files amongst [p]roject files' })
 vim.keymap.set('n', '<leader>tc', builtin.quickfix, { desc = '[t]elescope quickfix list [c]' })
 vim.keymap.set('n', '<leader>tq', builtin.command_history, { desc = '[t]elescope command history [q]' })
 vim.keymap.set('n', '<leader>tl', builtin.loclist, { desc = '[t]elescope [l]oclist' })
 vim.keymap.set('n', '<leader>tr', builtin.registers, { desc = '[t]elescope [r]egisters' })
-vim.keymap.set('n', '<leader>tbb', builtin.buffers, { desc = '[t]elescope [b]uffers [b]' })
+vim.keymap.set('n', '<leader><space>', builtin.buffers, { desc = '[ ] find existing buffers' })
 vim.keymap.set(
   'n',
-  '<leader>tbf',
+  '<leader>/',
   builtin.current_buffer_fuzzy_find,
-  { desc = '[t]elescope current [b]uffer [f]uzzy find' }
+  { desc = '[/] fuzzily search in current buffer' }
 )
-vim.keymap.set('n', '<leader>td', builtin.lsp_document_symbols, { desc = '[t]elescope lsp [d]ocument symbols' })
+vim.keymap.set('n', '<leader>tds', builtin.lsp_document_symbols, { desc = '[t]elescope lsp [d]ocument [s]ymbols' })
 vim.keymap.set(
   'n',
-  '<leader>to',
+  '<leader>tws',
   builtin.lsp_dynamic_workspace_symbols,
-  { desc = '[t]elescope lsp dynamic w[o]rkspace symbols' }
+  { desc = '[t]elescope lsp dynamic [w]orkspace [s]ymbols' }
 )
+
+-- Search from Visual mode
+function vim.getVisualSelection()
+  vim.cmd('noau normal! "vy"')
+  local text = vim.fn.getreg('v')
+  vim.fn.setreg('v', {})
+
+  text = string.gsub(text, "\n", "")
+  if #text > 0 then
+    return text
+  else
+    return ''
+  end
+end
+vim.keymap.set('v', '<leader>sg', function()
+  local text = vim.getVisualSelection()
+  builtin.live_grep({ default_text = text })
+end, { desc = '[s]earch via [g]rep for current selection' })
+vim.keymap.set('v', '<leader>/', function()
+  local text = vim.getVisualSelection()
+  builtin.current_buffer_fuzzy_find({ default_text = text })
+end, { desc = '[/] fuzzily search for current selection in current buffer' })
+
+vim.keymap.set('n', '<leader>t', builtin.treesitter, { desc = 'search [t]ags' })
 
 telescope.setup {
   defaults = {
@@ -110,7 +134,7 @@ telescope.setup {
       'truncate',
     },
     layout_strategy = 'vertical',
-    layout_config = layout_config,
+    -- layout_config = layout_config,
     mappings = {
       i = {
         ['<C-q>'] = actions.send_to_qflist,
@@ -118,9 +142,11 @@ telescope.setup {
         -- ['<esc>'] = actions.close,
         ['<C-s>'] = actions.cycle_previewers_next,
         ['<C-a>'] = actions.cycle_previewers_prev,
+        ['<M-p>'] = require('telescope.actions.layout').toggle_preview,
       },
       n = {
         q = actions.close,
+        ['<M-p>'] = require('telescope.actions.layout').toggle_preview,
       },
     },
     preview = {
@@ -133,7 +159,7 @@ telescope.setup {
     color_devicons = true,
     set_env = { ['COLORTERM'] = 'truecolor' },
     prompt_prefix = '   ',
-    selection_caret = '  ',
+    -- selection_caret = '  ',
     entry_prefix = '  ',
     initial_mode = 'insert',
     vimgrep_arguments = {
