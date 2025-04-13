23cd91a5e353d7927a3358ed833337324fb2d11a
achuie <achuie@protonmail.com>
2025-04-14 02:51:33 +0900

    Configure orgmode.
    * Capture templates for scratch, task, and journal
    * Try to make cal and agenda start on Sunday
    * Postpone task template to have a default deadline of the end of the
      week; harder than I thought because nvim doesn't have the emacs date
      arithmetic

--- nvim/plugin/orgmode.lua
+++ nvim/plugin/orgmode.lua
@@ -5,7 +5,29 @@ vim.g.did_load_orgmode_plugin = true
 
 require('orgmode').setup({
   org_agenda_files = '~/orgfiles/**/*',
+  org_agenda_start_on_weekday = 0,
+  calendar_week_start_day = 0,
   org_default_notes_file = '~/orgfiles/refile.org',
+  org_capture_templates = {
+    s = {
+      description = 'Scratch',
+      template = '* Unnamed %u\n%?\n%a',
+    },
+    t = {
+      description = 'Task',
+      template = '* TODO %?\n  DEADLINE: %t\n%a\n%u',
+      target = '~/orgfiles/todo.org',
+    },
+    j = {
+      description = 'Journal',
+      template = '\n*** %<%Y-%m-%d> %<%A>\n**** %U\n%?\n',
+      target = '~/orgfiles/journal/%<%Y-%m>.org',
+      datetree = { reversed = true, tree_type = 'month' },
+    },
+  },
 })
 
-require('org-roam').setup({ directory = '~/orgfiles' })
+require('org-roam').setup({
+  directory = '~/orgfiles/roam',
+  org_files = { '~/orgfiles' },
+})
